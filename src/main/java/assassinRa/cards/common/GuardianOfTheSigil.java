2937ec02ac8b7e684a5eb7a41edc7709eac2d1e4
Mateo Calcagno <mateo55_malunies@hotmail.com>
2023-10-20 20:14:59 -0300

    Guardian of the Sigil card updated
    I finally figured out how to make the card work. player->energy->energy is the max amount (I think) and energyOnUse is the energy used.

--- src/main/java/assassinRa/cards/common/GuardianOfTheSigil.java
+++ src/main/java/assassinRa/cards/common/GuardianOfTheSigil.java
@@ -2,16 +2,11 @@ package assassinRa.cards.common;
 
 import assassinRa.cards.BaseCard;
 import assassinRa.character.AssassinCharacter;
-import assassinRa.powers.AssassinsMark;
 import assassinRa.util.CardStats;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 public class GuardianOfTheSigil extends BaseCard {
     public static final String ID = makeID(assassinRa.cards.common.GuardianOfTheSigil.class.getSimpleName());
@@ -20,22 +15,19 @@ public class GuardianOfTheSigil extends BaseCard {
             CardType.SKILL,
             CardRarity.COMMON,
             CardTarget.SELF,
-            1
+            0
     );
 
-    private static final int UPG_COST = 0;
 
     public GuardianOfTheSigil() {
         super(ID, info);
-        setCostUpgrade(UPG_COST);
         exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if(p.drawPile.isEmpty() || p.discardPile.isEmpty()){
-            int energyAmount = p.energy.energyMaster - p.energy.energy;
-            addToBot(new GainEnergyAction(energyAmount));
+            addToBot(new GainEnergyAction(p.energy.energy - energyOnUse));
         }
     }
 
